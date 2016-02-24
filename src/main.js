a80d1e427b30f4293589f3b38f8cf2d0dcfacfbd
Simone Busoli <simone.busoli@gmail.com>
2016-02-24 01:17:08 +0100

    relab changes

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ require('crash-reporter').start();
 var mainWindow = null;
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform != 'darwin') {
@@ -19,26 +19,24 @@ app.on('window-all-closed', function() {
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
   // Create the browser window.
-  mainWindow = new BrowserWindow(
-      {
-          width: 1280,
-          height: 720,
-          frame: false,
-          resizable:false,
-          alwaysOnTop:false
-      }
-  );
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    frame: false,
+    resizable: false,
+    alwaysOnTop: false
+  });
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
-  //mainWindow.openDevTools();
+   mainWindow.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
--- src/webcam.js
+++ src/webcam.js
@@ -1,117 +1,99 @@
 'use strict';
 
 var remote = require('remote');
+var spawn = require('child_process').spawn;
+var path = require('path');
 
-let videoSource = [];
-let videoIndex = 0;
-let videoSourceLength = 0;
+let videoDevices = [];
 
-window.onload=function(e){
-    init();
-    controls();
-    webcamPrep();
-}
+window.addEventListener('load', _ => {
+  dots();
+  controls();
+  webcamPrep();
+});
 
-function init(){
-    if (hasGetUserMedia()){
-        console.log('Ready to start!');
-    }else{
-        alert('getUserMedia() is not supported in your browser');
+function dots() {
+  var dots = [];
+
+  var child = spawn(path.resolve(__dirname, '../bin/eye-coords'));
+
+  child.stdout.setEncoding('utf-8');
+
+  child.stdout.on('data', data => {
+    var left_top = data.split(',').map(Number);
+
+    if (!left_top[0] || !left_top[1]) return;
+
+    var dot = document.createElement('div');
+    dot.className = 'dot';
+    dot.style.left = (left_top[0] * 100) + '%';
+    dot.style.top = (left_top[1] * 100) + '%';
+
+    dots.push(dot);
+    document.body.appendChild(dot);
+
+    if (dots.length > 5) {
+      document.body.removeChild(dots.shift());
     }
-
+  });
 }
 
-function controls(){
-    const close = document.querySelector('#closeButton');
-    const theatre = document.querySelector('#theatreMode');
-    const toggle = document.querySelector('#toggleVideoSource');
-    const window = remote.getCurrentWindow();
+function controls() {
+  const close = document.querySelector('.closeButton');
+  const theatre = document.querySelector('.theatreMode');
+  const toggle = document.querySelector('.toggleVideoSource');
+  const window = remote.getCurrentWindow();
 
-    toggle.addEventListener(
-        'click',
-        function(e){
-            videoIndex++;
-            playVideo();
-        }
-    )
+  toggle.addEventListener('click', playVideo);
 
-    close.addEventListener(
-        'click',
-        function(e){
-            window.close();
-        }
-    );
+  close.addEventListener('click', _ => window.close());
 
-    theatreMode.addEventListener(
-        'click',
-        function(e){
-            var window = remote.getCurrentWindow();
-            if (!window.isFullScreen()){
-                window.setResizable(true);
-                window.setFullScreen(true);
-            }else{
-                window.setFullScreen(false);
-                window.setResizable(false);
-            }
+  theatre.addEventListener(
+      'click',
+      function (e) {
+        var window = remote.getCurrentWindow();
+        if (!window.isFullScreen()) {
+          window.setResizable(true);
+          window.setFullScreen(true);
+        } else {
+          window.setFullScreen(false);
+          window.setResizable(false);
         }
-    );
+      }
+  );
 }
 
-function webcamPrep(){
-    MediaStreamTrack.getSources(
-        function(sourceInfos){
-            var tempVideoIndex = 0;
-
-            for (let i = 0; i != sourceInfos.length; ++i){
-                //console.log(sourceInfos[i]);
-                if (sourceInfos[i].kind === 'video') {
-                    //console.log('video source found: ', sourceInfos);
-                    videoSource[tempVideoIndex] = sourceInfos[i];
-                    videoSourceLength++;
-                    tempVideoIndex++;
-
-                }
-            }
-            playVideo();
-        }
-    );
+function webcamPrep() {
+  navigator.mediaDevices.enumerateDevices().then(function (devices) {
+    videoDevices = devices.filter(d => d.kind === 'videoinput');
+    playVideo();
+  });
 }
 
-function errorCallback(err){
-    console.log('Rejected', err);
+function errorCallback(err) {
+  console.log('Rejected', err);
 }
 
-function successCallback(stream){
-    const video = document.querySelector('#liveVideo');
-    video.src = window.URL.createObjectURL(stream);
+function successCallback(stream) {
+  const video = document.querySelector('.liveVideo');
+  video.src = window.URL.createObjectURL(stream);
 }
 
-function playVideo(){
+function playVideo() {
+  // rotate devices
+  var video = videoDevices.shift();
+  videoDevices.push(video);
 
-    videoIndex = videoIndex % videoSourceLength;
-    console.log('current video source :',videoSource[videoIndex]);
-    const constraints = {
-        audio: false,
-        video: {
-            mandatory: {
-                minWidth: 1280,
-                minHeight: 720,
-                sourceId:videoSource[videoIndex].id
-            }
-        }
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        minWidth: 1280,
+        minHeight: 720,
+        sourceId: video.deviceId
+      }
     }
+  };
 
-
-    navigator.webkitGetUserMedia(
-        constraints,
-        successCallback,
-        errorCallback
-    );
-
-}
-
-
-
-function hasGetUserMedia(){
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)
+  navigator.webkitGetUserMedia(constraints, successCallback, errorCallback);
 }
